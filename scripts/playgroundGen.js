87f9e6156075f27230a71e052f70bfd39f77cb32
rubenvanerp <revanerp@gmail.com>
2025-04-10 21:00:59 +0200

    .

--- scripts/playgroundGen.js
+++ scripts/playgroundGen.js
@@ -14,10 +14,24 @@ function createGenericPage(top,left){
     return page
 }
 
-function createGenericFormField(top,left,width,height){
+function createGenericFormField(top, left, width, height, color, alignment ){
+    // ====
+    //  Creates a form field box
+    // ====
+    // top, left, width, height speak for themselves
+    // alignment, choose from things like "left" "center"
+    // color, set the color, default is "#dde4ff"
+
+
+    //These allow variables to be undefined, and they set a default value
+    var alignment = (alignment === undefined) ? "left" : alignment;
+    var color = (color === undefined) ? "#dde4ff" : color;
+
+    console.log(alignment)
+
     const formfield = document.createElement("textarea");
     formfield.id = "testTextField";
-    formfield.style = "--top:"+String(top)+"px; --left:"+String(left)+"px; --width:"+String(width)+"px; --height:"+String(height)+"px; --color:"+String("#dde4ff")+";"
+    formfield.style = "--top:"+String(top)+"px; --left:"+String(left)+"px; --width:"+String(width)+"px; --height:"+String(height)+"px; --color:"+String(color)+"; --align:"+String(alignment)+""
     formfield.className = "genericformfield"
     formfield.spellcheck = false;
     return formfield
