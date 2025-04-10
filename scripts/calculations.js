6cb6789216703a91ae2f198fbf767a21bf5aea07
rubenvanerp <revanerp@gmail.com>
2025-04-10 23:21:17 +0200

    Calculation bones ready to have logic implemented

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -14,7 +14,6 @@ function enterCalc(){
    this.textContent = this.value
    this.value = dummy
 }
-
 function exitCalc(){
     //console.log("uncalculating "+this.id) //for testing
     let dummy = this.textContent
@@ -33,6 +32,8 @@ function calculate(formula){
 }
 
 export function calculationUpdater(){
+    //Will redo all the calculations and update all the calculated fields
+    //SHOULD NEVER BE CALLED WHEN USER IS EDITING A FORMFIELD
     let affectedFields = document.getElementsByClassName("calculated")
     for (let i = 0; i<affectedFields.length; i++){
         let field = affectedFields[i]
--- scripts/decoratorTemplate.js
+++ scripts/decoratorTemplate.js
@@ -10,6 +10,6 @@ export function genericDecoratorHandler(){
     for (let i = 0; i<affectedFields.length; i++){
         let field = affectedFields[i]
         //code to do something with field:
-        console.log(field.id)
+        console.log("Box height is: "+ getComputedStyle(field).getPropertyValue('--height').trim() + "   (this was printed by decoratorTemplate as an example)")
     }
 }
