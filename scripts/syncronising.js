e7f08c39862480d7f9fdca8d85033ab01c89a22f
rubenvanerp <revanerp@gmail.com>
2025-04-11 14:44:05 +0200

    Minor improvements

--- scripts/syncronising.js
+++ scripts/syncronising.js
@@ -6,15 +6,15 @@ export function syncDecorator(syncClass,field){
     className += " " + "sync" + " syncMe" + String(syncClass);
     syncClasses.push(String(syncClass));
     field.className = className;
-    field.addEventListener("focusout", syncDecoratorHandler, false);
+    field.addEventListener("focusout", syncUpdater, false);
     return field;
 };
 
 
-export function syncDecoratorHandler(){
+export function syncUpdater(){
     let classname = this.className
     let preSliced = classname.slice(this.className.indexOf("syncMe")) // filtering the syncclass from within the enitre class string
-    let syncClass = (preSliced.indexOf(" ") == -1) ? preSliced : preSliced.slice(0, preSliced.indexOf(" ")) // finishing filtering
+    let syncClass = (preSliced.indexOf(" ") == -1) ? preSliced : preSliced.slice(0, preSliced.indexOf(" ")) // finishing filtering (this is a one line if-else)
     
     let affectedFields = document.getElementsByClassName(syncClass)
     for (let i = 0; i<affectedFields.length; i++){
